34474f9262ef9ebe536e40fe3f3ccae1591f31e2
Micky-Doll <michaelaleedollman@gmail.com>
2023-10-30 22:55:28 +0200

    IWA7 Challenge 2

---
+++ IWA7/Challenge 2/scripts.js
@@ -0,0 +1,24 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+// Ternary operator ? is shorthand for if... else statements. Keeps things concise. Requires practice to remember.
+// condition ? expressionIfTrue : expressionIfFalse
+
+const message = nickname
+  ? `Good Morning, ${nickname}!`
+  : firstname
+  ? `Good Morning, ${firstname}!`
+  : "Good Morning!";
+console.log(message);
+
+// && - combines two or more conditions. Both must be true or false.
+
+// if (nickname && firstname) {
+//     console.log(`Good Morning, ${nickname}!`);
+//   } else if (nickname) {
+//     console.log(`Good Morning, ${nickname}!`);
+//   } else if (firstname) {
+//     console.log(`Good Morning, ${firstname}!`);
+//   } else {
+//     console.log("Good Morning!");
+//  }
