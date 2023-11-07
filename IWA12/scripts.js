b80319bdeace9b8af89138ac28560a0ba4e1ea8d
Micky-Doll <michaelaleedollman@gmail.com>
2023-11-07 13:29:43 +0200

    IWA12 AKA How many times can I get the MIME error by incorrectly linking my JS

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -1,63 +1,62 @@
 // scripts.js
 
+// NOTE: When they said the buttons need to be black and greyscale only, they meant the physical block and its outlining.
+// The point was for the text to be in the appropriate colours based on STATUS_MAP. *Almost* stepped into that spiral there.
 const STATUS_MAP = {
-    shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    },
-    checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    }
-}
+  shelf: {
+    color: "green",
+    canReserve: true,
+    canCheckout: true,
+    canCheckIn: false,
+  },
+  reserved: {
+    color: "blue",
+    canReserve: false,
+    canCheckout: true,
+    canCheckIn: false,
+  },
+  overdue: {
+    color: "red",
+    canReserve: false,
+    canCheckout: false,
+    canCheckIn: true,
+  },
+  checkedOut: {
+    color: "orange",
+    canReserve: false,
+    canCheckout: false,
+    canCheckIn: true,
+  },
+};
 
-// Edit below line 
+// Edit below line
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const books = document.querySelectorAll('div[id^="book"]');
+// caret operator is used to match elements whose attribute value is specified
+// select divs whose id attribute starts with the value 'book'
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+// Already referencing const books; just getting info from the children not the parent to program the buttons
+// empty or underscore is fine, or 'books'
+books.forEach(() => {
+  const statusElement = book.querySelector(".status");
+  const reserveButton = book.querySelector(".reserve");
+  const checkoutButton = book.querySelector(".checkout");
+  const checkinButton = book.querySelector(".checkin");
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+  const statusText = statusElement.textContent.trim().replace("Status:", "");
+  const statusDisplay = STATUS_MAP[statusText];
 
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+  // Updates statusElement colour with statusDisplay info found in STATUS_MAP.
+  if (statusDisplay) {
+    statusElement.style.color = statusDisplay.color;
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+    // Set it disabled first then check if it should be enabled. ! (light switch) operator makes it either/or
+    // so if canCheckIn is true !canCheckIn will be false or the inverse.
+    reserveButton.disabled = !statusDisplay.canReserve;
+    checkoutButton.disabled = !statusDisplay.canCheckout;
+    checkinButton.disabled = !statusDisplay.canCheckIn;
+  }
+});
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// It's been 40 minutes and the word status has stopped sounding like a word
+// Stop. Miss-linking. Your goddamn. JAVASCRIPT.
